5e219299b74e9355e52a95c511d4443696c424a0
kurtfriedrichhotmail <kurtfriedrich@hotmail.com>
2019-01-05 03:21:01 -0800

    added functioning buttons with dummy code

---
+++ nodetest1/public/javascripts/main.js
@@ -0,0 +1,10 @@
+var createButton = document.getElementById("createButton");
+var submitButton = document.getElementById("submitButton");
+
+createButton.addEventListener('click',function(){
+    alert("hello");
+});
+
+submitButton.addEventListener('click',function(){
+    alert("hello");
+});
